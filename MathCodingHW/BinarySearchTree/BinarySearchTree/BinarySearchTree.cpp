bde8221cf012e5009c7ec5a4e7cc9f83e6b3da0c
Heedong Lee <arkinylee@gmail.com>
2017-04-11 00:28:47 +0900

    Comment Added

--- MathCodingHW/BinarySearchTree/BinarySearchTree/BinarySearchTree.cpp
+++ MathCodingHW/BinarySearchTree/BinarySearchTree/BinarySearchTree.cpp
@@ -8,25 +8,26 @@ inline Node::~Node()
 
 void Node::Insert(int InValue)
 {
-	if (InValue <= value)
+	
+	if (InValue <= value)// 자기의 값보다 작거나 같으면 왼쪽으로 이동하거나 추가한다.
 	{
-		if (Node* LeftNode = GetLeft())
+		if (Node* LeftNode = GetLeft()) // 이미 왼쪽이 있으면 왼쪽에 넣어주는 시도를 한다.
 		{
 			LeftNode->Insert(InValue);
 		}
-		else
-		{
+		else // 왼쪽에 없으면 새로 노드를 만들어주고 추가해준다.
+		{			
 			LeftNode = new Node(InValue);
 			SetLeft(LeftNode);
 		}
 	}
-	else
+	else // 자기 값보다 크면 오른쪽으로 이동하거나 추가한다.
 	{
-		if (Node* RightNode = GetRight())
+		if (Node* RightNode = GetRight()) // 오른쪽 노드가 있으면 오른쪽 노드로 이동한다.
 		{
 			RightNode->Insert(InValue);
 		}
-		else
+		else // 없으면 노드를 만들어주고 추가해준다.
 		{
 			RightNode = new Node(InValue);
 			SetRight(RightNode);
@@ -36,14 +37,14 @@ void Node::Insert(int InValue)
 
 void Node::Print()
 {
-	if (Node* LeftNode = GetLeft())
+	if (Node* LeftNode = GetLeft()) // 왼쪽 노드가 있으면 왼쪽으로 이동한다.
 	{
 		LeftNode->Print();
 	}
 	
-	std::cout << value << ",";
+	std::cout << value << ","; // 왼쪽 처리가 끝나면 자기를 처리해준다.
 
-	if (Node* RightNode = GetRight())
+	if (Node* RightNode = GetRight()) // 오른쪽 노드가 있다면 이제 오른쪽으로 이동한다.
 	{
 		RightNode->Print();
 	}
@@ -51,23 +52,23 @@ void Node::Print()
 
 void Node::Destroy()
 {
-	if (Node* LeftNode = GetLeft())
+	if (Node* LeftNode = GetLeft()) // 왼쪽 노드가 있다면 들어가서 지워준다.
 	{
 		LeftNode->Destroy();
 		SetLeft(nullptr);
 	}
-	if (Node* RightNode = GetRight())
+	if (Node* RightNode = GetRight()) // 오른쪽 노드가 있으면 들어가서 지워준다.
 	{
 		RightNode->Destroy();
 		SetRight(nullptr);
 	}
 
-	delete this;
+	delete this; // 양쪽다 지웠으니 나도 지워도 된다.
 }
 
 void BinarySearchTree::Insert(int Value)
 {
-	if (pRoot == nullptr)
+	if (pRoot == nullptr) // 루트가 없다면 루트부터 새로 만든다.
 	{
 		pRoot = new Node(Value);
 	}
